dc53314616df8f9f343a27e0dfc65afd9fa3957f
HDDALI003 <HDDALI003@BSG034>
2019-04-11 10:38:17 +0200

    created new project

---
+++ FoodOutlet/FoodOutlet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodOutlet.Startup))]
+namespace FoodOutlet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
